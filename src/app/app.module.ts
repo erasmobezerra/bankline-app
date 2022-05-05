f9768dc0e01927cdc139ae9f971c701fa994a13f
erasmobezerra <erasmo.ads.tech@gmail.com>
2022-05-05 18:22:21 -0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovimentacaoListComponent } from './components/movimentacao-list/movimentacao-list.component';
+import { MovimentacaoNewComponent } from './components/movimentacao-new/movimentacao-new.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+import { CurrencyMaskModule } from "ng2-currency-mask";
+
+// Para formatação de valores do tipo data e monetário por exemplo
+registerLocaleData(localePt);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovimentacaoListComponent,
+    MovimentacaoNewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    //Como iremos trabalhar com requisições HTTPs e formulário, já vamos adicionando duas dependências no projeto.
+    FormsModule, 
+    HttpClientModule,
+    CurrencyMaskModule
   ],
-  providers: [],
+  // Para formatação de valores do tipo data e monetário por exemplo
+  providers: [{provide: LOCALE_ID, useValue: 'pt-BR'}],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
+
