07f3c5c66285632443039e40b642897f09f9ec4e
erasmobezerra <erasmo.ads.tech@gmail.com>
2022-05-06 02:17:14 -0300

    environment.API

--- src/app/services/correntista.service.ts
+++ src/app/services/correntista.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 
-const baseUrl = 'https://dio-bankline-api-erasmo.herokuapp.com'
+const baseUrl = `${environment.API}`
 
 @Injectable({
   providedIn: 'root'
